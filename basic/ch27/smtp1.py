fd8552c07b51b652223993aaab2e29e952468dbc
Heegene <dev.heejin@gmail.com>
2020-08-28 11:47:48 +0900

    200828 mailing test

---
+++ basic/ch27/smtp1.py
@@ -0,0 +1,30 @@
+# mail message 사용 객체 선언
+
+from email.message import EmailMessage
+import smtplib
+
+# SSL로 open(SMTP 객체 생성
+smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+# smtp 정상여부 hello test
+
+smtp.ehlo()
+
+# login
+smtp.login('dev.heejin@gmail.com', '앱비밀번호넣기')
+
+# message 생성
+msg = EmailMessage()
+
+# 메일내용 내부적으로 dic 선언
+msg['Subject'] = '파이썬 mailing test'
+msg['From'] = 'dev.heejin@gmail.com'
+msg['To'] = 'dev.heejin@gmail.com'
+msg.set_content('''
+                    청주 얼굴맛집 이콩이!
+                    콩이는 얼굴천재다
+                    face genius KongE
+                ''')
+
+# 메일전송
+smtp.send_message(msg)
+smtp.quit()
